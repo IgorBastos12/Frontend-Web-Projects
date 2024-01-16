8faab7024cd636c5e1edfe027cb90d2dcf0fa0b9
IgorBastos12 <igordebastos98@hotmail.com>
2024-01-16 17:30:06 -0300

    finalização do jogo da memoria

--- Games/Jogo da memoria/assets/js/game.js
+++ Games/Jogo da memoria/assets/js/game.js
@@ -1,114 +1,125 @@
 const game = {
-
   lockMode: false,
   memoryFirstCard: null,
   memorySecundCard: null,
 
-  
-
   techs: [
-          'bootstrap', 'css', 
-          'electron', 'firebase', 
-          'html', 'javascript', 
-          'jquery', 'mongo', 
-          'node', 'react'
-        ],
-
+    "bootstrap",
+    "css",
+    "electron",
+    "firebase",
+    "html",
+    "javascript",
+    "jquery",
+    "mongo",
+    "node",
+    "react",
+  ],
 
   cards: null,
 
+  createCards: function () {
+    // Método que gera as cartas já embaralhadas
 
+    this.cards = []; //adiciono um array para propriedade cards
 
-  createCards: function  () {       // Método que gera as cartas já embaralhadas
-
-    this.cards = [];  //adiciono um array para propriedade cards
-
-    for(let tech of this.techs) {
-
-      this.cards.push(this.CardInformation(tech));  // Para cada tech geramos um array com 2 objetos dentro com informações da tech, essas informações serão as informações da carta, e adicionamos esse array no array na propriedade cards
-
+    for (let tech of this.techs) {
+      this.cards.push(this.CardInformation(tech)); // Para cada tech geramos um array com 2 objetos dentro com informações da tech, essas informações serão as informações da carta, e adicionamos esse array no array na propriedade cards
     }
 
-    this.cards = this.cards.flatMap(pair => pair)    // flapMap desmembra os arrays que tem outros arrays dentro, com isso nossa propriedade cards recebe apenas o novo array desmembrado 
-    
-    this.shuffleCards();  // Após isso chamamos nosso método para embaralhar carta,  ele pega o novo array demebrado de cards e mistura os elementos.
+    this.cards = this.cards.flatMap((pair) => pair); // flapMap desmembra os arrays que tem outros arrays dentro, com isso nossa propriedade cards recebe apenas o novo array desmembrado
 
+    this.shuffleCards(); // Após isso chamamos nosso método para embaralhar carta,  ele pega o novo array demebrado de cards e mistura os elementos.
 
     console.log(this.cards);
-
   },
 
+  CardInformation: function (tech) {
+    // Método que gera as informações do par de cartas
 
-  CardInformation: function  (tech) {  // Método que gera as informações do par de cartas
-
-    pairCards = [{
-      id:this.createId(tech),
-      tech: tech,
-      flipped: false
-
-    },{
-      id: this.createId(tech),
-      tech: tech,
-      flipped: false
-
-    }];
+    pairCards = [
+      {
+        id: this.createId(tech),
+        tech: tech,
+        flipped: false,
+      },
+      {
+        id: this.createId(tech),
+        tech: tech,
+        flipped: false,
+      },
+    ];
     return pairCards;
   },
 
-  createId: function (tech) {  // Método que gera os ids das cartas
+  createId: function (tech) {
+    // Método que gera os ids das cartas
 
     let id = tech + parseInt(Math.random() * 10000);
-    
-    return id;
 
+    return id;
   },
 
-  shuffleCards: function () {  // Método que embaralha as cartas
+  shuffleCards: function () {
+    // Método que embaralha as cartas
 
     let indexArray = 0;
-    
 
-    for (indexArray = 0; indexArray < this.cards.length; indexArray++ ) {
+    for (indexArray = 0; indexArray < this.cards.length; indexArray++) {
+      let randomIndex = Math.floor(Math.random() * 20); // Gera um num aleátorio entre 0 e 19
 
-      let randomIndex = Math.floor(Math.random() * 20);  // Gera um num aleátorio entre 0 e 19
-
-      [this.cards[indexArray], this.cards[randomIndex]] = [this.cards[randomIndex],this.cards[indexArray]]
+      [this.cards[indexArray], this.cards[randomIndex]] = [
+        this.cards[randomIndex],
+        this.cards[indexArray],
+      ];
     }
-
   },
 
-  
-  setPairCards: function(e){
+  setPairCards: function (e) {
+    let setCard = this.cards.filter((card) => card.id == e.id)[0];
 
-    let setCard = this.cards.filter(card => card.id == e.id)[0];
+    if (setCard.flipped || this.lockMode) {
+      return false;
+    }
 
-
-    if(this.memoryFirstCard == null) {
+    if (this.memoryFirstCard == null) {
       this.memoryFirstCard = setCard;
+      this.memoryFirstCard.flipped = true;
 
+      return true;
     } else {
       this.memorySecundCard = setCard;
+      this.memorySecundCard.flipped = true;
+
       this.lockMode = true;
-  
-      
+      return true;
     }
-    
-  }, 
+  },
 
-  checkPair:  function () {
-
-    return this.memoryFirstCard.tech == this.memorySecundCard.tech;
+  checkPair: function () {
+    if (this.memoryFirstCard == null || this.memorySecundCard == null) {
+      return false;
+    } else {
+      return this.memoryFirstCard.tech == this.memorySecundCard.tech;
+    }
   },
 
   clear: function () {
-
     this.memoryFirstCard = null;
     this.memorySecundCard = null;
     this.lockMode = false;
+  },
 
-  }
+  clearFlipped: function () {
+    this.memoryFirstCard.flipped = false;
+    this.memorySecundCard.flipped = false;
 
+    this.clear();
+  },
 
+  checkGameOver: function () {
+    let notFlip = this.cards.filter((card) => card.flipped == false);
 
-}
-
+    return notFlip.length;
+  },
+};
--- Games/Jogo da memoria/assets/js/script.js
+++ Games/Jogo da memoria/assets/js/script.js
@@ -1,99 +1,87 @@
-const FRONT = 'cardFront';
-const BACK = 'cardBack';
-const CARD = 'card';
-const ICON = 'icon'
+const FRONT = "cardFront";
+const BACK = "cardBack";
+const CARD = "card";
+const ICON = "icon";
 
 startGame();
 
-
 function startGame() {
-
-    game.createCards();
-    addCardsScreen();
-    ClickandPlay()
-    
+  game.createCards();
+  addCardsScreen();
+  ClickandPlay();
 }
 
-
 function addCardsScreen() {
+  let gameBoard = document.querySelector(".gameBoard");
 
-    let gameBoard = document.querySelector('.gameBoard');
+  for (let tech of game.cards) {
+    let card = document.createElement("div"); // Cria a 1ª div com a classe card e adiciona o id dela
+    card.classList.add(CARD);
+    card.id = tech.id;
+    card.setAttribute(ICON, tech.tech);
 
-    for (let tech of game.cards) {
+    let cardFront = document.createElement("div"); // Cria a div com a classe cardFront
+    cardFront.classList.add(FRONT);
 
+    let img = document.createElement("img"); // Cria a tag img
+    img.src = `./assets/images/${tech.tech}.png`; // Adiciona o src da imagem
 
-        let card = document.createElement('div');       // Cria a 1ª div com a classe card e adiciona o id dela
-        card.classList.add(CARD);
-        card.id = tech.id;
-        card.setAttribute(ICON, tech.tech);
-        
+    let cardBack = document.createElement("div"); // Cria a div com a classe cardBack
+    cardBack.classList.add(BACK);
+    cardBack.innerHTML = "&lt;/&gt;"; // Adiciona o texto dentro da div cardBack
 
-        let cardFront = document.createElement('div');   // Cria a div com a classe cardFront
-        cardFront.classList.add(FRONT);
-        
-        let img = document.createElement('img');       // Cria a tag img
-        img.src = `./assets/images/${tech.tech}.png`; // Adiciona o src da imagem
-
-        let cardBack = document.createElement('div');   // Cria a div com a classe cardBack
-        cardBack.classList.add(BACK);
-        cardBack.innerHTML = '&lt;/&gt;';       // Adiciona o texto dentro da div cardBack
-
-
-
-        cardFront.appendChild(img);       // Adiciona a tag img dentro da div cardFront 
-        card.appendChild(cardFront);     // Adiciona a div cardFront dentro da div card
-        card.appendChild(cardBack);     // Adiciona a div cardBack dentro da div card
-        gameBoard.appendChild(card);   // Adiciona a div card dentro da div gameBoard
-
-    }
-
+    cardFront.appendChild(img); // Adiciona a tag img dentro da div cardFront
+    card.appendChild(cardFront); // Adiciona a div cardFront dentro da div card
+    card.appendChild(cardBack); // Adiciona a div cardBack dentro da div card
+    gameBoard.appendChild(card); // Adiciona a div card dentro da div gameBoard
+  }
 }
 
 function ClickandPlay() {
+  let cards = window.document.getElementsByClassName(CARD);
 
-    let cards = window.document.getElementsByClassName(CARD);
-
-    for (let card of cards) {
-        card.addEventListener('click' , () => {
-            
-            if(game.lockMode == false){
-                card.classList.add('flip');
-                game.setPairCards(card);
-            
-            }
-            
-            if (game.lockMode == true) {
-
-                if(game.checkPair() == false) {
-
-
-                    setTimeout(() => {
-                        
-                        let firstCardView = document.getElementById(game.memoryFirstCard.id);
-                        let secundCardView = document.getElementById(game.memorySecundCard.id);
-
-                        firstCardView.classList.remove('flip');
-                        secundCardView.classList.remove('flip');
-
-                        game.clear();
-
-                    }, 1000);
-                    
-                    
-                
-                } else {
-
-
-                    game.clear();
-                }
-            }
-
-                        
-        });
-    }   
-
+  for (let card of cards) {
+    card.addEventListener("click", played);
+  }
 }
 
+function played() {
+  if (game.setPairCards(this)) {
+    this.classList.add("flip");
 
+    if (game.memorySecundCard != null) {
+      if (game.checkPair()) {
+        game.clear();
 
+        if (game.checkGameOver() == 0) {
+          let gameOver = document.getElementById("gameOver");
 
+          gameOver.style.visibility = "visible";
+
+          restart();
+        }
+      } else {
+        let firstCardView = document.getElementById(game.memoryFirstCard.id);
+
+        let secundCardView = document.getElementById(game.memorySecundCard.id);
+
+        setTimeout(() => {
+          firstCardView.classList.remove("flip");
+          secundCardView.classList.remove("flip");
+
+          game.clearFlipped();
+        }, 1000);
+      }
+    }
+  }
+}
+
+function restart() {
+  let btnRestart = document.getElementsByClassName("btnRestart")[0];
+
+  console.log(btnRestart);
+
+  btnRestart.addEventListener("click", () => {
+    location.reload(true);
+  });
+}
